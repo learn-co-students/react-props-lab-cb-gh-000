5c4a72eb5b7f1eb1c976dd094b11a5a5a92e1c73
Thomas Tuts <thomas@thomastuts.com>
2016-08-25 12:33:53 +0200

    Rework narrative to Star Wars theme

--- README.md
+++ README.md
@@ -5,29 +5,29 @@
 2. Override default props with your own prop values
 
 ## Overview
-![Mad Max](https://media.giphy.com/media/84bpBkUVOsJ8Y/giphy.gif)
+![Millennium Falcon](https://media.giphy.com/media/4kL3Q4lIggnGU/giphy.gif)
 
-Let's say we're a War Boy going to a local Fury Road car meetup. Since we're going to be talking about our
-post-apocalyptic murder machines on wheels, we need to represent them somehow. That's where you come in! You'll be
-creating a React component that shows us some info on a given car.
+Let's say we're a spaceship captain going to a local Star Wars meetup. Since we want to showcase the awesome ships
+people are commandeering, we need to represent them somehow. That's where you come in! You'll be creating a React
+component that shows us some info on a given spaceship.
 
-1. In the `components/Car.js` file, create a `Car` React component
+1. In the `components/Spaceship.js` file, create a `Spaceship` React component
 2. This component has several props: 
     1. `name` (string)
-    2. `horsepower` (number)
+    2. `speed` (number)
     3. `hasRockets` (boolean, defaults to `false`)
     4. `colors` (array of strings, defaults to `['black', 'red']`)
     
 Feel free to render out the data in any form you wish!
 
-Note: you'll need to _export_ the `Car` component, like this:
+Note: you'll need to _export_ the `Spaceship` component, like this:
 
 ```js
-module.exports = class Car extends React.Component { ... };
+module.exports = class Spaceship extends React.Component { ... };
 // OR
 module.exports = React.createClass({ ... });
 // OR declare your component first, and then:
-module.exports = Car;
+module.exports = Spaceship;
 ```
  
 ## Resources
--- test/index-test.js
+++ test/index-test.js
@@ -1,38 +1,38 @@
 const React = require('react');
 const { shallow } = require('enzyme');
 
-const Car = require('../components/Car');
+const Spaceship = require('../components/Spaceship');
 
-describe('<Car />', () => {
+describe('<Spaceship />', () => {
   describe('Props', function () {
 
     describe('name', function () {
       it('should render a `name` prop', () => {
-        const wrapper = shallow(<Car name="WarBoyTruck" />);
-        expect(wrapper.text().includes('WarBoyTruck')).toBeTruthy('The `name` prop is not being used in the render() function.');
+        const wrapper = shallow(<Spaceship name="Millennium Falcon" />);
+        expect(wrapper.text().includes('Millennium Falcon')).toBeTruthy('The `name` prop is not being used in the render() function.');
       });
     });
 
-    describe('horsepower', function () {
-      it('should render a `horsepower` prop', () => {
-        const wrapper = shallow(<Car horsepower={500} />);
-        expect(wrapper.text().includes(500)).toBeTruthy('The `horsepower` prop is not being used in the render() function.');
+    describe('speed', function () {
+      it('should render a `speed` prop', () => {
+        const wrapper = shallow(<Spaceship speed={500} />);
+        expect(wrapper.text().includes(500)).toBeTruthy('The `speed` prop is not being used in the render() function.');
       });
     });
 
     describe('hasRockets', function () {
       it('should default to false', () => {
-        expect(Car.defaultProps.hasRockets).toEqual(false);
+        expect(Spaceship.defaultProps.hasRockets).toEqual(false);
       });
     });
 
     describe('colors', function () {
       it("should default to `['black', 'red']`", () => {
-        expect(Car.defaultProps.colors).toEqual(['black', 'red']);
+        expect(Spaceship.defaultProps.colors).toEqual(['black', 'red']);
       });
 
       it('should render the given colors', () => {
-        const wrapper = shallow(<Car colors={['green', 'magenta']} />);
+        const wrapper = shallow(<Spaceship colors={['green', 'magenta']} />);
         expect(wrapper.text().includes('green')).toBeTruthy('The `colors` prop is not being used in the render() function.');
         expect(wrapper.text().includes('magenta')).toBeTruthy('The `colors` prop is not being used in the render() function.');
       });
