13faed42edf4d919e6a1d2d148639d761e65e05c
Luke Ghenco <lukeghenco@gmail.com>
2017-06-05 07:18:17 -0400

    Update test/root.js to use recommended setup

--- test/root.js
+++ test/root.js
@@ -1,17 +1,13 @@
-const path = require('path');
-const jsdom = require('jsdom').jsdom;
-const fs = require('fs');
+require('babel-register')();
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'));
-const exposedProperties = ['window', 'navigator', 'document'];
+var exposedProperties = ['window', 'navigator', 'document'];
 
-global.expect = require('expect');
-global.document = jsdom(html);
+var jsdom = require('jsdom').jsdom;
+
+global.document = jsdom('');
 global.window = document.defaultView;
-
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
-    exposedProperties.push(property);
     global[property] = document.defaultView[property];
   }
 });
