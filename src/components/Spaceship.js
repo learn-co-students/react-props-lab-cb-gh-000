214f212906f1c462477ea1cd39506bee00b234a5
Abdelrauof <abdelrauof97@gmail.com>
2018-11-17 14:23:48 +0200

    Done

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default class SpaceShip extends React.Component {
+export default class SpaceShip extends Component {
   render () {
     return (
       <div className="space-ship">
