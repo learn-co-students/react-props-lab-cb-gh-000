d34428ba074a14080e7bc87d3977b37537b89955
Bianca Zanardi <bianca.zanardi@gmail.com>
2018-12-30 11:12:32 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,9 +1,15 @@
+<<<<<<< HEAD
 import React from 'react';
 
 class Spaceship extends React.Component {
 
   render() {
     const hasRockets = this.props.hasRockets;
+=======
+class Spaceship extends React.Component {
+  const hasRockets = this.props.hasRockets;
+  render() {
+>>>>>>> 31f1eba5024bdd70dcfc2f87bd53a397915ea29e
     return (
       <div className="spaceship-card">
         <h2>{this.props.name}</h2>
@@ -21,4 +27,8 @@ Spaceship.defaultProps = {
   colors: ['black', 'red'],
 };
 
+<<<<<<< HEAD
 export default Spaceship;
+=======
+const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+>>>>>>> 31f1eba5024bdd70dcfc2f87bd53a397915ea29e
